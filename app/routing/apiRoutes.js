3977220d848177ca321a206cbceadb01f3cc8df2
Nelson Quinones <nelson.quinones@yahoo.com>
2018-05-12 12:22:14 -0400

    Adding new files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,54 +1,53 @@
 
-
 //getting data from friends.js
-var friends = require("./data/friends.js");
+var friends = require("../data/friends.js");
 
 //console.log (friends[0].name);
 
 module.exports = function(app){
 
 //setting route to display json data.
-app.get("/app/friends.js", function(req, res){
-    response.json(friends);
+app.get("/api/friends", function(req, res){
+    res.json(friends);
 });
-app.post("/app/data/friends.js", function(req, res){
+app.post("/api/friends", function(req, res){
     var newFriend = request.body;
 //console.log(friends[0].score[0]);
 })
 //console.log(newFriends);
 
-for(var i = 0; i < newFriend.scores.length; i++){
-    if(newFriend.score[i] === "1 (Strongly Disagree)"){
-        newFriend.scores[i] =1;
-    }else if(newFriend.scores[i] === "5 (Strongly Agree)"){
-        newFriend.scores[i] = 5;
-    }else {
-        newFriend.scores[i] = parseInt(newFriend.scores[i]);
-    }
-}
+// for(var i = 0; i < newFriend.scores.length; i++){
+//     if(newFriend.score[i] === "1 (Strongly Disagree)"){
+//         newFriend.scores[i] =1;
+//     }else if(newFriend.scores[i] === "5 (Strongly Agree)"){
+//         newFriend.scores[i] = 5;
+//     }else {
+//         newFriend.scores[i] = parseInt(newFriend.scores[i]);
+//     }
+// }
 
 
-var different = [];
+// var different = [];
 
-for (var i = 0; i < friends.length; i++){
-    var compatibleFriend = friends[i];
-    var notCompatible = 0;
+// for (var i = 0; i < friends.length; i++){
+//     var compatibleFriend = friends[i];
+//     var notCompatible = 0;
 
-for(var f = 0; f < compatibleFriend.scores.length; f++){
-    var someCompatible = Math.abs(compatibleFriend.scores[f] - newFriend.scores[f]);
-    notCompatible += someCompatible;
-}
-diff.push(notCompatible);
-}
-var number = different[0];
-var index = 0;
+// for(var f = 0; f < compatibleFriend.scores.length; f++){
+//     var someCompatible = Math.abs(compatibleFriend.scores[f] - newFriend.scores[f]);
+//     notCompatible += someCompatible;
+// }
+// diff.push(notCompatible);
+// }
+// var number = different[0];
+// var index = 0;
 
-    for(var z = 0; z < different.length; z++){
-        if(different[z] < number){
-            number = different[z];
-        index = z;
-    }
-}
-friends.push(newFriend);
-response.json(friends[index]);
+//     for(var z = 0; z < different.length; z++){
+//         if(different[z] < number){
+//             number = different[z];
+//         index = z;
+//     }
+// }
+// friends.push(newFriend);
+// response.json(friends[index]);
 };
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 
 
 require("./app/routing/htmlRoutes.js")(app);
-// require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
 
 
 // Starts the server to begin listening
